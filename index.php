ef9b037b1ea0147bcd367db545848181f05c59f4
strelchuk18 <strelchuk18@gmail.com>
2019-10-11 12:51:42 +0300

    Added calculation

--- index.php
+++ index.php
@@ -7,15 +7,21 @@
     <?php
       function func(&$var1, &$var2, $operation)
     {
+      switch ($operation) :
+        case "/": return $var1 / $var2; 
+        case "*": return $var1 * $var2;
+        case "+": return $var1 + $var2;
+        case "-": return $var1 - $var2;
+       endswitch;
       $res = $var1 . $operation . $var2;
-      return $res;
+        return $res;
     }
-      $a = 1;
-      $b = 2;
-      echo func($a, $b, "/");
-      echo func($a, $b, "*");
-      echo func($a, $b, "+");
-      echo func($a, $b, "-");
+      $a = 5;
+      $b = 10;
+      echo func($a, $b, '/');
+      echo func($a, $b, '*');
+      echo func($a, $b, '+');
+      echo func($a, $b, '-');
       echo $a;
       echo $b;
     ?>
