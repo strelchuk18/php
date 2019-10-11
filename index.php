581b1d13934af53a38f2d14817397cc92cf31c30
strelchuk18 <strelchuk18@gmail.com>
2019-10-11 09:20:41 +0300

    Fixed code

--- index.php
+++ index.php
@@ -7,20 +7,17 @@
     <?php
       function func(&$var1, &$var2, $operation)
     {
-      $res = $var2 / $var1;
-      $var1 = $var2 * $var1;
-      $var2 = $var2 + $var1;
-      $var2 = $var2 - $var1;
-      $operation = "/ * + -";
-          return $res;
+      $res = $var1 . $operation . $var2;
+      return $res;
     }
-      $a = 5;
-      $b = 10;
-      $operation = "/ * - +";
-      echo func($a, $b, $operation);
+      $a = 1;
+      $b = 2;
+      echo func($a, $b, "/");
+      echo func($a, $b, "*");
+      echo func($a, $b, "+");
+      echo func($a, $b, "-");
       echo $a;
       echo $b;
-      echo $operation;
     ?>
   </body>
 </html>
