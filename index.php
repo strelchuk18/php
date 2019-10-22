54669740cb5f98f6371f0021c6cd09df3fd0f05b
strelchuk18 <strelchuk18@gmail.com>
2019-10-22 13:34:52 +0300

    Fix function

--- index.php
+++ index.php
@@ -65,17 +65,45 @@
       filee($files);
       echo "<br />";
       
-      function team1($A, $B) {
-        asort($A);
-          foreach($A as $k=>$v) {
-            
-            echo "$k ". "<br />";
+      function team1($first, $second) {
+        asort($first);
+          foreach($first as $key=>$value)
+           {
+            echo "$key=>$value". "<br />";
           }
+          $eco = array ($first_team);
+          array_push($eco, $second_team);
+          echo($eco);
       }
-        $first_team = ['Dmytro' => 5, 'Ivan' => 1,'Vova' => 6, 'Andriy' => 3,'Vasyl' => 2,'Petro' => 4,'Kindrat' => 7,'Oleksandr' => 1,'Oksana' => 3,'Olena' => 5,'Kyrylo' => 3];
-        $second_team = ['Stepan' => 5, 'Ivan' => 1,'Vova' => 6,'Serhii' => 3,'Vasyl' => 2,'Petro' => 4,'Oleksiy' => 7,'Iruna' => 1,'Oksana' => 3,'Olena' => 5,'Filip' => 3];
-        $result = array_merge($first_team, $second_team);
-      team1($result, $result);
+        $first_team = [
+          'Dmytro' => 5, 
+          'Ivan' => 1,
+          'Vova' => 6, 
+          'Andriy' => 3,
+          'Vasyl' => 2,
+          'Petro' => 4,
+          'Kindrat' => 7,
+          'Ivan' => 1,
+          'Oleksandr' => 1,
+          'Oksana' => 3,
+          'Olena' => 5,
+          'Kyrylo' => 3
+        ];
+        $second_team = [
+          'Stepan' => 5, 
+          'Ivan' => 1,
+          'Vova' => 6,
+          'Serhii' => 3,
+          'Vasyl' => 2,
+          'Petro' => 4,
+          'Oleksiy' => 7,
+          'Iruna' => 1,
+          'Oksana' => 3,
+          'Olena' => 5,
+          'Filip' => 3
+        ];
+        $first = array_replace($first_team, $second_team);
+      team1($first, $second);
       echo "<br />";
     ?>
   </body>
